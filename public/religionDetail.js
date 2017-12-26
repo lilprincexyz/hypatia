d5f02ea0424dfe14bb0011e538c6d15a2c7b304c
Nylfmedli14 <f7vaapad@gmail.com>
2017-12-26 10:57:39 -0500

    added delete method for delete button

--- public/religionDetail.js
+++ public/religionDetail.js
@@ -1,11 +1,20 @@
-$(function(){
+$(function() {
 	const deleteButton = $("#deleteButton")
 	const religionId = deleteButton.data("religion-id")
+	const API_URL = "http://localhost:8080"
 
 	//add event click listener to delete button
-
+	deleteButton.on("click", function(event) {
+		event.preventDefault();
 	//in callback function, make jQuery ajax call with delete method
-
+	$.ajax({
+			method: "DELETE",
+			contentType: "application/json",
+			url: API_URL + "/religion/" + religionId,
+	}) 
 	//then redirect to religion listing page
-	
+	.then(res => {
+			window.location.href= "../religion/"
+		})
+	})
 })
